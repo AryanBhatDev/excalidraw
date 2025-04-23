73eef868660a5a18155d8b72728b7d390298f016
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-23 16:48:19 +0530

    Added token check in ws

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,8 +1,31 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
+const wss = new WebSocketServer({ port: 8080 });
 
-const wss = new WebSocketServer({port:8080});
+wss.on('connection', (socket, request) => {
+    const url = request.url;
 
-wss.on('connection',(socket)=>{
-    socket.send("hello you are connected to the websocket server")
-})
+    if (!url) {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token');
+    if (!token) {
+        return;
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded || !(decoded as JwtPayload)._id) {
+        socket.close();
+        return;
+    }
+
+    socket.on('message', (data) => {
+        socket.send('pong');
+    });
+});
