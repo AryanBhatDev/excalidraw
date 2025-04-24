51b7036813bb741d3dfe475149abf0888873ac91
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-24 18:59:47 +0530

    ws layer completed

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,31 +1,103 @@
-import { WebSocketServer } from 'ws';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import { WebSocket, WebSocketServer } from 'ws';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+import { prisma } from '@repo/db/client';
 
-const wss = new WebSocketServer({ port: 8080 });
+dotenv.config({path:path.resolve(__dirname,'../../../.env')})
 
-wss.on('connection', (socket, request) => {
+const wss = new WebSocketServer({ port: 8081 });
+
+interface User {
+    socket: WebSocket;
+    rooms: string[];
+    userId: string;
+}
+
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (typeof decoded == 'string') {
+            return null;
+        }
+
+        if (!decoded || !decoded.id) {
+            return null;
+        }
+        return decoded.id;
+
+    } catch (e) {
+        return null;
+    }
+}
+
+wss.on('connection', (socket, request)=> {
     const url = request.url;
-
     if (!url) {
         return;
     }
     const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token');
-    if (!token) {
-        return;
-    }
+    const token = queryParams.get('token') || '';
+    const userId = checkUser(token);
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (!decoded || !(decoded as JwtPayload)._id) {
+    if (userId == null) {
         socket.close();
-        return;
+        return null;
     }
+    users.push({
+        userId,
+        rooms: [],
+        socket,
+    });
 
-    socket.on('message', (data) => {
-        socket.send('pong');
+    socket.on('message', async(data)=> {
+        let parsedData;
+        if (typeof data !== 'string') {
+            parsedData = JSON.parse(data.toString());
+        } else {
+            parsedData = JSON.parse(data);
+        }
+
+        if (parsedData.type === 'join_room') {
+            const user = users.find((x) => x.socket === socket);
+            user?.rooms.push(parsedData.roomId);
+        }
+
+        if (parsedData.type === 'leave_room') {
+            const user = users.find((x) => x.socket === socket);
+            if (!user) {
+                return;
+            }
+            user.rooms = user?.rooms.filter((x) => x === parsedData.room);
+        }
+
+
+        if (parsedData.type === 'chat') {
+            const roomId = parsedData.roomId;
+            const message = parsedData.message;
+
+            await prisma.chat.create({
+                data: {
+                    roomId: Number(roomId),
+                    message,
+                    userId,
+                },
+            });
+
+            users.forEach((user) => {
+                if (user.rooms.includes(roomId)) {
+                    user.socket.send(
+                        JSON.stringify({
+                            type: 'chat',
+                            message: message,
+                            roomId,
+                        })
+                    );
+                }
+            });
+        }
     });
 });
